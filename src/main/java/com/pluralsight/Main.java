51c5d278eb0670d7082bade87528b5202f2821f1
JammingJams <noahlogan20o00@gmail.com>
2026-05-06 14:47:19 -0400

    Completed the file formatter!

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,7 +3,8 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
 
-        NameFormatter.format(" nO   a   H  ","  l  O  g  a  N  ");
-        NameFormatter.format("mR", " nO   a   H  ", " ja K e ", "  l  O  g  a  N  ", " coL");
+        NameFormatter.format(" noaH  ","  logaN  ");
+        NameFormatter.format("mR", " nOaH  ", " jake ", "  lOgaN  ", "coL");
+        NameFormatter.format("cOol nAme jOhn");
     }
 }
--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -1,26 +1,30 @@
 package com.pluralsight;
 
 public class NameFormatter {
-    private String firstName, lastName, middleName, prefix, suffix;
-
     public static String format(String firstName, String lastName) {
-        firstName = firstName.trim().replaceAll("\\s+", "").substring(0,1).toUpperCase() + firstName.trim().replaceAll("\\s+", "").toLowerCase().substring(1);
-        lastName = lastName.trim().replaceAll("\\s+", "").substring(0,1).toUpperCase() + lastName.trim().replaceAll("\\s+", "").toLowerCase().substring(1);
+        firstName = firstName.trim().substring(0,1).toUpperCase() + firstName.trim().toLowerCase().substring(1);
+        lastName = lastName.trim().substring(0,1).toUpperCase() + lastName.trim().toLowerCase().substring(1);
         System.out.printf("%s %s\n", firstName, lastName);
         return firstName + " " + lastName;
     }
 
     public static String format(String prefix, String firstName, String middleName, String lastName, String suffix) {
-        prefix = prefix.trim().replaceAll("\\s+", "").substring(0,1).toUpperCase() + prefix.trim().replaceAll("\\s+", "").toLowerCase().substring(1);
-        firstName = firstName.trim().replaceAll("\\s+", "").substring(0,1).toUpperCase() + firstName.trim().replaceAll("\\s+", "").toLowerCase().substring(1);
-        middleName = middleName.trim().replaceAll("\\s+", "").substring(0,1).toUpperCase() + middleName.trim().replaceAll("\\s+", "").toLowerCase().substring(1);
-        lastName = lastName.trim().replaceAll("\\s+", "").substring(0,1).toUpperCase() + lastName.trim().replaceAll("\\s+", "").toLowerCase().substring(1);
-        suffix = suffix.trim().replaceAll("\\s+", "").substring(0,1).toUpperCase() + suffix.trim().replaceAll("\\s+", "").toLowerCase().substring(1);
+        prefix = prefix.trim().substring(0,1).toUpperCase() + prefix.trim().toLowerCase().substring(1);
+        firstName = firstName.trim().substring(0,1).toUpperCase() + firstName.trim().toLowerCase().substring(1);
+        middleName = middleName.trim().substring(0,1).toUpperCase() + middleName.trim().toLowerCase().substring(1);
+        lastName = lastName.trim().substring(0,1).toUpperCase() + lastName.trim().toLowerCase().substring(1);
+        suffix = suffix.trim().substring(0,1).toUpperCase() + suffix.trim().toLowerCase().substring(1);
         System.out.printf("%s. %s %s %s, %s\n", prefix, firstName, middleName, lastName, suffix);
         return prefix + ". " + firstName + " " + middleName + " " + lastName + ", " + suffix;
     }
 
     public static String format(String fullName) {
+        String[] tempArray = fullName.split(" ");
+        String extra = "";
+        for (String x : tempArray) {
+            extra = extra + x.substring(0,1).toUpperCase() + x.substring(1).toLowerCase() + " ";
+        }
+        System.out.println(extra);
         return fullName;
     }
 
